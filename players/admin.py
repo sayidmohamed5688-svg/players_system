4734464ee0f660cedfc0fd21fc2b31c09626c94f
sayid <sayidmohammed5688@gmail.com>
2026-07-04 13:33:11 +0300

    l

--- players/admin.py
+++ players/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
-from.models import Player
+from.models import Player , Team ,Match , News 
 
 admin.site.register(Player)
+admin.site.register(Match)
+admin.site.register(Team)
+admin.site.register(News)
+
+
